4e6188f9eed56555e7313f42aa78f31dd76db394
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2022-08-04 00:40:15 +0530

    project setup

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
+from .views import Home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Home),
 ]
+urlpatterns+=static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
